d808a50c426dd7d7e948368c1fe4f82e8f7df3d1
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-06 01:40:08 +0200

    UI design 🎨
    added navigation ⛵️

---
+++ app/src/main/java/com/ikarimeister/myrentalappartments/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ikarimeister.myrentalappartments.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ikarimeister.myrentalappartments.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
