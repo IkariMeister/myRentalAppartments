d060b80c8c5e611c5af0030a9955ef2947cdf05a
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-09 17:21:26 +0200

    Updating Android CI Badge 🥇

---
+++ app/src/main/java/com/ikarimeister/myrentalappartments/ui/fragment/AccommodationDetail.kt
@@ -0,0 +1,25 @@
+package com.ikarimeister.myrentalappartments.ui.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ikarimeister.myrentalappartments.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AccommodationDetail : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_accommodation_detail, container, false)
+    }
+
+
+}
