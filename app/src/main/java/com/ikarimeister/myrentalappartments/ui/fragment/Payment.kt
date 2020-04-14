c337bf1a79e9bc8b4064bf28e2bb97ba00ad6630
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-15 00:08:55 +0200

    Added payment_area.xml UI Design 🎨💰

---
+++ app/src/main/java/com/ikarimeister/myrentalappartments/ui/fragment/Payment.kt
@@ -0,0 +1,14 @@
+package com.ikarimeister.myrentalappartments.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ikarimeister.myrentalappartments.R
+
+class Payment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.payment_area, container, false)
+    }
+}
